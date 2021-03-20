b993b5142acbdc149ab8cbf1d8b2713ee9871169
Nathan Chua <natekchua@gmail.com>
2021-03-20 00:16:17 -0600

    use papaparse to read local dataset, chartJS cookie component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from 'react';
 import Home from './components/Home/Home';
+import Chart1 from './components/AMCharts/Chart1/Chart1';
 
 import './App.css';
 
+const csv = require('./nba2020-modified-v2.csv');
+const papa = require('papaparse/papaparse.min.js');
+
 function App () {
+  const [dataset, setDataset] = useState(null);
+  
+  useEffect(() => {
+    papa.parse(csv.default, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      complete: setData
+    });
+  }, [])
+  
+  const setData = (result) => {
+    setDataset(result.data);
+  }
+
   return (
     <div className='App'>
       <Home />
+      <Chart1 dataset={dataset} />
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+
 import './home.css';
 
 function Home () {
   return (
-    <header className='home-header'>
+    <header className='page-container home-bg'>
       <div className='home-intro-text'>
         <h2>Welcome to <span className='primary'>Shooters Shoot.</span></h2>
         <h3>An interactive data visualization for 2020 NBA Shooting Statistics.</h3>
+      </div>
+      <div className='btn-container'>
         <Button variant='contained' color='primary'>
-          GO
+          Variation 1
+        </Button>
+        <Button variant='contained' color='primary'>
+          Variation 2
+        </Button>
+        <Button variant='contained' color='primary'>
+          Variation 3
         </Button>
       </div>
-  </header>
+    </header>
   );
 }
 
