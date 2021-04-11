5145c2e2f53dd883378fadd2fca281a918f2276c
Nathan Chua <natekchua@gmail.com>
2021-04-10 18:29:15 -0600

    added filter menu

--- src/components/Charts/ShotChart.jsx
+++ src/components/Charts/ShotChart.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Link } from 'react-scroll';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Slider from '@material-ui/core/Slider';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import './chart.css';
+
+const barChartColors = require('./barChartColors.js');
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -13,6 +20,10 @@ const useStyles = makeStyles((theme) => ({
     width: '100px',
     height: '50px'
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 180,
+  }
 }));
 
 const chartOptions = {
@@ -41,9 +52,23 @@ const chartOptions = {
   } 
 }
 
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement='top' title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
 function ShotChart (props) {
   const { dataset } = props;
   const classes = useStyles();
+  const [position, setPosition] = useState('Point Guard');
+  const [category, setCategory] = useState('Field Goals');
+  const [sortBy, setSortBy] = useState('Percentage');
+  const [sortOrder, setSortOrder] = useState('None');
+  const [numPlayers, setNumPlayers] = useState(5);
 
   let testSubset3, playerLabels3, chartData3, chartData3b;
   const xAxisLabels = ['FG Made', 'FG Attempted'];
@@ -56,8 +81,7 @@ function ShotChart (props) {
     chartData3 = {
       labels: xAxisLabels,
       datasets: testSubset3.map(fp => [fp.FG, fp.FGA]).map((stat, idx) => {
-        // Random color generator: https://css-tricks.com/snippets/javascript/random-hex-color/
-        const color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        const color = barChartColors.colorArr[idx];
         return {
           label: playerLabels3[idx],
           backgroundColor: color,
@@ -85,7 +109,7 @@ function ShotChart (props) {
   }
 
   return (
-    <div className='page-container chart-bg-odd' id='shot-chart'>
+    <div className='position-container chart-bg-odd' id='shot-chart'>
       <div className='chart-container'>
         <div className='horizontal-flex'>
           <div style={{ flex: 0.6 }}>
@@ -131,19 +155,98 @@ function ShotChart (props) {
                 } 
               }}
             />
-          </div>
-          
+          </div>      
         </div>
         <div className='horizontal-flex'>
-          <p>
-            This variation uses a sample subset that is represented through a vertically grouped bar chart.
-            The x-axis separates each group by shooting rows "FG Made", "FG Attempted", and "FG%" where "FG" means "Field Goal".
-            Similar to the first variation, The color coded legend on the right hand side allows users to visually determine distinct players and their percentages.
-            The main difference between this variation and the first one, is that the percentage column is isolated onto it's own chart and scale. I think 
-            this variation makes more since for the given dataset since field goals will have a higher value of makes and attempts in comparison to 3-pointers and free throws.
-            By isolating field goal percentage, it is easier to visually interpret these percentage values.
-          </p>
-          <Link
+          <div className='horizontal-flex filter-container'>
+            <h4>Filters:</h4>
+            <FormControl variant='filled' className={classes.formControl}>
+              <InputLabel htmlFor='filled-position-native-simple'>Position</InputLabel>
+              <Select
+                native
+                value={position}
+                onChange={e => setPosition(e.target.value)}
+                label='Position'
+                inputProps={{
+                  name: 'position',
+                  id: 'outlined-position-native-simple',
+                }}
+              >
+                <option aria-label='None' value='' />
+                <option value={'Point Guard'}>Point Guard</option>
+                <option value={'Shooting Guard'}>Shooting Guard</option>
+                <option value={'Small Forward'}>Small Forward</option>
+                <option value={'Power Forward'}>Power Forward</option>
+                <option value={'Center'}>Center</option>
+              </Select>
+            </FormControl>
+            <FormControl variant='filled' className={classes.formControl}>
+              <InputLabel htmlFor='filled-position-native-simple'>Statistic Category</InputLabel>
+              <Select
+                native
+                value={category}
+                onChange={e => setCategory(e.target.value)}
+                label='Category'
+                inputProps={{
+                  name: 'Category',
+                  id: 'outlined-position-native-simple',
+                }}
+              >
+                <option aria-label='None' value='' />
+                <option value={'Field Goals'}>Field Goals</option>
+                <option value={'Three Pointers'}>Three Pointers</option>
+                <option value={'Free Throws'}>Free Throws</option>
+              </Select>
+            </FormControl>
+            <FormControl variant='filled' className={classes.formControl}>
+              <InputLabel htmlFor='filled-position-native-simple'>Sort By</InputLabel>
+              <Select
+                native
+                value={sortBy}
+                onChange={e => setSortBy(e.target.value)}
+                label='Sorting'
+                inputProps={{
+                  name: 'Sorting',
+                  id: 'outlined-position-native-simple',
+                }}
+              >
+                <option aria-label='None' value='' />
+                <option value={'Percentage'}>Percentage</option>
+                <option value={'Makes'}>Makes</option>
+                <option value={'Attempts'}>Attempts</option>
+              </Select>
+            </FormControl>
+            <FormControl variant='filled' className={classes.formControl}>
+              <InputLabel htmlFor='filled-position-native-simple'>Sort Order</InputLabel>
+              <Select
+                native
+                value={sortOrder}
+                onChange={e => setSortOrder(e.target.value)}
+                label='Sorting'
+                inputProps={{
+                  name: 'Sorting',
+                  id: 'outlined-position-native-simple',
+                }}
+              >
+                <option aria-label='None' value='' />
+                <option value={'None'}>None</option>
+                <option value={'Ascending'}>Ascending</option>
+                <option value={'Descending'}>Descending</option>
+              </Select>
+            </FormControl>
+            <h5 className='filter-item'># of Players</h5>
+            <Slider
+              ValueLabelComponent={ValueLabelComponent}
+              aria-label='# of Players'
+              defaultValue={5}
+              min={5}
+              max={15}
+              onChange={e => setNumPlayers(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+         <Link
             activeClass='active'
             to='chart-2'
             spy={true}
---
+++ src/components/Charts/barChartColors.js
@@ -0,0 +1,17 @@
+exports.colorArr = [
+  '#00FFFF',
+  '#7FFF00',
+  '#DC143C',
+  '#FF8C00',
+  '#FF00FF',
+  '#4B0082',
+  '#20B2AA',
+  '#0000CD',
+  '#8B4513',
+  '#6A5ACD',
+  '#FFFF00',
+  '#C0C0C0',
+  '#DA70D6',
+  '#1E90FF',
+  '#BDB76B'
+]
